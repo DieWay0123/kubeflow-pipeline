d90bf611b0427e21f3d31a9b934a33c7c6260abd
DieWay0123 <85541962+DieWay0123@users.noreply.github.com>
2024-07-23 03:08:55 +0800

    add some comment & change dataset path

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-FILE_PATH="10k.csv"
+FILE_PATH="/dataset/10k.csv" #the path you save your dataset
 URL="https://raw.githubusercontent.com/daniel88516/diabetes-data/main/10k.csv"
 
+#check if the dataset is already existed
 if [ ! -f "$FILE_PATH" ]; then
     wget -O $FILE_PATH $URL
 else
